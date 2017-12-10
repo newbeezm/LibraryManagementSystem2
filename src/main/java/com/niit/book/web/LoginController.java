2097d34c0f587a61b833c10d62390f95e7d01fff
newbeezm <805243475@qq.com>
2017-12-10 17:41:22 +0800

    remove useless comments

--- src/main/java/com/niit/book/web/LoginController.java
+++ src/main/java/com/niit/book/web/LoginController.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
-//标注为一个Spring mvc的Controller
 @Controller
 public class LoginController {
 
